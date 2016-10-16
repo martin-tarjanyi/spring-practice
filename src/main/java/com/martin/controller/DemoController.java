345087ef341e42a5ba0312038a5a9c37c6110598
Martin Tarjanyi <tmartin193@gmail.com>
2016-10-16 17:04:53 +0200

    initial commit

---
+++ src/main/java/com/martin/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.martin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController
+{
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
+
